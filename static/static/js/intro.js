5a96dff8b304ce2bf0432db590b5f7e1bd11d5d0
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-03 22:13:33 +0200

    [add] preload images

--- static/static/js/intro.js
+++ static/static/js/intro.js
@@ -23,3 +23,27 @@ window.onload = () => {
             img.src = array[i];
         }
     }
+
+
+    preloadImages(["/static/intro/img-1.jpg",
+        "/static/intro/img-2.jpg",
+        "/static/intro/img-3.jpg",
+        "/static/intro/img-4.jpg",
+        "/static/intro/img-5.jpg",
+        "/static/intro/img-6.jpg",
+        "/static/intro/img-7.jpg",
+        "/static/intro/img-8.jpg",
+        "/static/intro/img-9.jpg",
+        "/static/intro/img-10.jpg",
+        "/static/intro/img-11.jpg",
+        "/static/intro/img-12.jpg",
+        "/static/intro/img-13.jpg",
+        "/static/intro/img-14.jpg",
+        "/static/intro/img-15.jpg",
+        "/static/intro/img-16.jpg",
+        "/static/intro/img-17.jpg",
+        "/static/intro/img-18.jpg",
+        "/static/intro/img-19.jpg",
+        "/static/intro/img-20.jpg"]);
+
+ }
