b30834fb2bb6141e7025c0ba2107b85949f6b787
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-05 22:59:50 +0200

    [add] featured movies route

--- static/js/cinematic_universe.js
+++ static/js/cinematic_universe.js
@@ -1,24 +1,30 @@
 import {dataHandler} from "./data_handler.js";
 
 
-const characterContainer = document.querySelector("#character_grid_container");
-const url = "/get_mcu_movies";
+const moviesContainer = document.querySelector("#movies_container");
+const featuredContainer = document.querySelector("#featured_movies_container");
 
-const fetchMCUCharacters = () => {
-    dataHandler._api_get(url, createCards);
+const allMovies = "/get_mcu_movies";
+const featured = "/get_featured_movies";
+
+const fetchMCUMovies = () => {
+    dataHandler._api_get(allMovies, createCards);
 }
 
+const fetchFeaturedMovies = () => {
+    dataHandler._api_get(featured, createFeaturedCards);
+}
 
-const createCards = (movies) => {
-    for (let movie of movies) {
+function createFeaturedCards(movies) {
+     for (let movie of movies) {
         if (movie.cover_url!==null){
-            characterContainer.insertAdjacentHTML('beforeend', `
+            featuredContainer.insertAdjacentHTML('beforeend', `
             <article class="gradient-shadow">
-                <div class="card card_details_container">
+                <div class="featured_card card_details_container">
                    <img src="${movie.cover_url}" alt="${movie.title}" class="card_img">
                     <div class="descriptions">
                         <h2 class="card__title">${movie.title}</h2>
-                        <p className = "overview">` +
+                        <p class="overview">` +
                         (movie.overview !== null ? movie.overview : 'No info') +
                         `</p>                  
                     </div>
@@ -30,4 +36,25 @@ const createCards = (movies) => {
     }
 }
 
-fetchMCUCharacters();
+const createCards = (movies) => {
+    for (let movie of movies) {
+        if (movie.cover_url!==null){
+            moviesContainer.insertAdjacentHTML('beforeend', `
+            <article class="gradient-shadow">
+                <div class="card card_details_container">
+                   <img src="${movie.cover_url}" alt="${movie.title}" class="card_img">
+                    <div class="descriptions">
+                        <h2 class="card__title">${movie.title}</h2>
+                        <p class="overview">` +
+                        (movie.overview !== null ? movie.overview : 'No info') +
+                        `</p>                  
+                    </div>
+                </div>
+            </article>
+        `);
+        }
+
+    }
+}
+fetchFeaturedMovies();
+fetchMCUMovies();
