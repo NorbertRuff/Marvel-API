96e09b905b94383341611c57a777c3306386d1e0
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-03 21:03:51 +0200

    [add] dummy test

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,18 +1,6 @@
-# Third party modules
-import pytest
-
-# First party modules
-from app import create_app
+def inc(x):
+    return x + 1
 
 
-@pytest.fixture
-def client():
-    application = create_app()
-    application.config["TESTING"] = True
-    with application.test_client() as client:
-        yield client
-
-
-def test_square(client):
-    rv = client.get("/square?number=8")
-    assert b"64" == rv.data
+def test_answer():
+    assert inc(4) == 5
